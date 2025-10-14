66340154e0d494fd0c4154d67d15a2a902da9b83
Trikzy7 <theoplebani2002@icloud.com>
2025-10-14 13:19:21 +0200

    First Commit

---
+++ src/main/java/com/example/tp_cbtw_java_watch_shop/TpCbtwJavaWatchShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_cbtw_java_watch_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpCbtwJavaWatchShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpCbtwJavaWatchShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tp_cbtw_java_watch_shop/TpCbtwJavaWatchShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp_cbtw_java_watch_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpCbtwJavaWatchShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
